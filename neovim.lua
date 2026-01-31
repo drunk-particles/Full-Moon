5d912c14aed3809d0866385eeb07f1a1f9a3c8e8
Samin <adnanbr786@gmail.com>
2026-01-31 22:56:56 +0600

    theme update: Sat Jan 31 10:56:56 PM +06 2026

--- neovim.lua
+++ neovim.lua
@@ -1,15 +1,12 @@
 return {
-	{
-		"kdheepak/monochrome.nvim",
-		priority = 1000,
-		config = function()
-			vim.o.background = "light"
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "monochrome",
-		},
-	},
+  {
+    "kepano/flexoki-neovim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "flexoki-light",
+    },
+  },
 }
